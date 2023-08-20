7d4bafbc127f799b89ec4f0ba86b577bfa6d94a3
Christian Kim Abac <abackim9087@gmail.com>
2023-08-20 11:31:39 +0800

    new update

--- newAlgo/func.coversation.php
+++ newAlgo/func.coversation.php
@@ -1,5 +1,5 @@
 <?php
-
+// this class aims to modify the user answer based on given algorithm
 class Conversation{
     public $answer;
     public $qNum;
@@ -9,79 +9,6 @@ class Conversation{
     protected static $questionAlgoArr2 = array(3,5,7,11,13,15,17,21,23,24,25,28,29,31,32,34,37,41,42,45,47,48,51,53,54,55,56,57,61,62,65,67,68,71,72,73,77,78,85,88,94,95,97,101,103,105,106,107,108,112,113,114,115,117,118,122,127,132,133,134,135,142,143,145,146,147,150);
 
     protected static $questionAlgoArr3 = array(10,20,30,40,50,60,70,80,90,100,110,120,121,123,130,140);
-
-    protected static $personalityElementsArr = array(
-        [
-            'A'=>'A 근면성',
-            'questionSet'=>[24,25,34,44,54,64,74,84,94,104,114,124,134,144,212,226],
-            'answerShape'=>[24=>'',25=>'',34=>'',44=>'',54=>'',64=>'',74=>'',84=>'',94=>'',104=>'',114=>'',124=>'',134=>'',144=>'',212=>'',226=>''],
-            'shapeModification'=>[],
-            'modificaitonConversionToActual'=>[]
-        ],
-        [
-            'B'=>'B 책임감',
-            'questionSet'=>[6,16,26,36,46,56,66,76,96,106,116,126,136,146,137,208,217,221],
-            'answerShape'=>[6=>'',16=>'',26=>'',36=>'',46=>'',56=>'',66=>'',76=>'',96=>'',106=>'',116=>'',126=>'',136=>'',146=>'',137=>'',208=>'',217=>'',221=>''],
-            'shapeModification'=>[],
-            'modificaitonConversionToActual'=>[]
-        ],
-        [
-            'C'=>'C 협동성',
-            'questionSet'=>[8,18,28,33,38,39,48,58,68,78,98,108,128,132,148,225,223],
-            'answerShape'=>[8=>'',18=>'',28=>'',33=>'',38=>'',39=>'',48=>'',58=>'',68=>'',78=>'',98=>'',108=>'',128=>'',132=>'',148=>'',225=>'',223=>''],
-            'shapeModification'=>[],
-            'modificaitonConversionToActual'=>[]
-        ],
-        [
-            'D'=>'D 자주성',
-            'questionSet'=>[1,11,14,21,31,41,51,71,81,91,101,111,118,131,141,220,224],
-            'answerShape'=>[1=>'',11=>'',14=>'',21=>'',31=>'',41=>'',51=>'',71=>'',81=>'',91=>'',101=>'',111=>'',118=>'',131=>'',141=>'',220=>'',224=>''],
-            'shapeModification'=>[],
-            'modificaitonConversionToActual'=>[]
-        ],
-        [
-            'E'=>'E 지도성',
-            'questionSet'=>[9,19,29,49,69,79,89,99,109,119,129,138,139,149,150],
-            'answerShape'=>[9,19,29,49,69,79,89,99,109,119,129,138,139,149,150],
-            'shapeModification'=>[],
-            'modificaitonConversionToActual'=>[]
-        ],
-        [
-            'F'=>'F 감정상태',
-            'questionSet'=>[5,17,27,37,47,55,57,61,67,77,97,107,117,127,147,214,228,209,216,213],
-            'answerShape'=>[5,17,27,37,47,55,57,61,67,77,97,107,117,127,147,214,228,209,216,213],
-            'shapeModification'=>[],
-            'modificaitonConversionToActual'=>[]
-        ],
-        [
-            'G'=>'G 집중력',
-            'questionSet'=>[7,15,35,45,65,75,85,95,105,115,125,55,135,145,87,215,219,227],
-            'answerShape'=>[7,15,35,45,65,75,85,95,105,115,125,55,135,145,87,215,219,227],
-            'shapeModification'=>[],
-            'modificaitonConversionToActual'=>[]
-        ],
-        [
-            'H'=>'H 정서안정',
-            'questionSet'=>[3,13,23,43,53,63,73,83,93,103113,59,133,132,143,210,222],
-            'answerShape'=>[3,13,23,43,53,63,73,83,93,103113,59,133,132,143,210,222],
-            'shapeModification'=>[],
-            'modificaitonConversionToActual'=>[]
-        ],
-        [
-            'I'=>'I 준법성',
-            'questionSet'=>[2,12,22,32,42,52,62,72,82,88,92,102,112,122,142,218,207,211],
-            'answerShape'=>[2,12,22,32,42,52,62,72,82,88,92,102,112,122,142,218,207,211],
-            'shapeModification'=>[],
-            'modificaitonConversionToActual'=>[]
-        ]
-    );
-    protected static $correctAnswer = [];
-    protected static $testMakerModification = [];
-    protected static $answerShape = [];
-    protected static $shapeModification = [];
-    protected static $numberOfCorrectAnswers = [];
-    protected static $convertOfValue = [];
-    protected static $theNumberOfHits = [];
 
     public function __constructor($inputNum,$inputAnswer){
         $this->answer = $inputAnswer;
